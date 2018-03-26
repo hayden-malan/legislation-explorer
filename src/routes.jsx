32b0ac15af70a5d1e4185c3b0578bb61768fa45a
Anna-Livia <annalivia.gomart@gmail.com>
2018-03-26 12:02:31 +0200

    change routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,20 +7,26 @@ import SwaggerPage from "./components/pages/swagger"
 import NotFoundPage from "./components/pages/not-found"
 import ParameterOrVariablePage from "./components/pages/parameter-or-variable"
 
+// specify basename below if running in a subdirectory or set as "/" if app runs in root
+const appHistory = useRouterHistory(createHistory)({
+  basename: "/tmp"
+});
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    <Route path="swagger" component={SwaggerPage}/>
-    <Route path=":name" component={ParameterOrVariablePage}/>
-    <Route path="parameters">
-      <IndexRedirect to="/" />
-      <Redirect from=":name" to="/:name" />
+  <Router history={appHistory} />
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} />
+      <Route path="swagger" component={SwaggerPage}/>
+      <Route path=":name" component={ParameterOrVariablePage}/>
+      <Route path="parameters">
+        <IndexRedirect to="/" />
+        <Redirect from=":name" to="/:name" />
+      </Route>
+      <Route path="variables">
+        <IndexRedirect to="/" />
+        <Redirect from=":name" to="/:name" />
+      </Route>
+      <Route path="*" component={NotFoundPage} />
     </Route>
-    <Route path="variables">
-      <IndexRedirect to="/" />
-      <Redirect from=":name" to="/:name" />
-    </Route>
-    <Route path="*" component={NotFoundPage} />
-  </Route>
+  </Router>
 )
