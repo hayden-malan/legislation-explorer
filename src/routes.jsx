f056450236b710215e757978a6775c33fdf732a4
Anna-Livia <annalivia.gomart@gmail.com>
2018-03-27 17:47:19 +0200

    New routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Redirect, Route, IndexRedirect, IndexRoute} from "react-router"
+import {Router, Redirect, Route, IndexRedirect, IndexRoute} from "react-router"
 
 import App from "./components/app"
 import HomePage from "./components/pages/home"
@@ -9,20 +9,20 @@ import ParameterOrVariablePage from "./components/pages/parameter-or-variable"
 
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    <Route path="swagger" component={SwaggerPage}/>
-    <Route path=":name" component={ParameterOrVariablePage}/>
-    <Route path="parameters">
-      <IndexRedirect to="/" />
-      <Redirect from=":name" to="/:name" />
   <Router basename="/tmp">
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} />
+      <Route path="tmp/swagger" component={SwaggerPage}/>
+      <Route path="tmp/:name" component={ParameterOrVariablePage}/>
+      <Route path="parameters">
+        <IndexRedirect to="/" />
+        <Redirect from=":name" to="/:name" />
+      </Route>
+      <Route path="variables">
+        <IndexRedirect to="/" />
+        <Redirect from=":name" to="/:name" />
+      </Route>
+      <Route path="*" component={NotFoundPage} />
     </Route>
-    <Route path="variables">
-      <IndexRedirect to="/" />
-      <Redirect from=":name" to="/:name" />
-    </Route>
-    <Route path="*" component={NotFoundPage} />
-  </Route>
   </Router>
 )
