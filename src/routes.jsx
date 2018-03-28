5b2606c2bd5e64fae86a2995d3f82d56b4890a12
Anna-Livia <annalivia.gomart@gmail.com>
2018-03-28 11:19:35 +0200

    back to start

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,7 +10,7 @@ import ParameterOrVariablePage from "./components/pages/parameter-or-variable"
 
 export default (
   <Router basename="/tmp">
-    <Route path="/tmp" component={App}>
+    <Route path="/" component={App}>
       <IndexRoute component={HomePage} />
       <Route path="/tmp/swagger" component={SwaggerPage}/>
       <Route path="/tmp/:name" component={ParameterOrVariablePage}/>
@@ -22,11 +22,7 @@ export default (
         <IndexRedirect to="/" />
         <Redirect from=":name" to="/:name" />
       </Route>
-      <Route path="/">
-        <IndexRedirect to="/tmp" />
-        <Redirect from=":name" to="/:name" />
       </Route>
-      <Route path="*" component={NotFoundPage} />
-    </Route>
+    <Route path="*" component={NotFoundPage} />
   </Router>
 )
